104c6fded080df1b0526e6002586eb31e5a004d8
ngontjar <54755292+Gontjarow@users.noreply.github.com>
2020-09-18 23:38:50 +0300

    Made set_pixel much safer.

--- pixel.c
+++ pixel.c
@@ -3,24 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   pixel.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: krusthol <krusthol@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: ngontjar <ngontjar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 15:51:22 by krusthol          #+#    #+#             */
-/*   Updated: 2020/09/15 15:56:27 by krusthol         ###   ########.fr       */
+/*   Updated: 2020/09/18 23:37:21 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom_nukem.h"
 
-void 	set_pixel(SDL_Surface *buff, int x, int y, uint32_t color)
+void	set_pixel(SDL_Surface *buff, int x, int y, uint32_t color)
 {
 	unsigned int	*pixels;
-	int 			i;
-	int 			j;
+	int				i;
+	int				j;
+	int				index;
 
 	i = x;
-	j = buff->w * y;
-	pixels = buff->pixels;
-	pixels[i + j] = color;
+	j = y * buff->w;
+	index = i + j;
+	if ((index >= 0) && (index < buff->w * buff->h))
+	{
+		pixels = buff->pixels;
+		pixels[index] = color;
+	}
 }
-
