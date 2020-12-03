9188703d491034d56e9a5e9138051884f75ee91a
Niko Gontjarow <niko.gontjarow@gmail.com>
2020-12-03 20:19:21 +0200

    Oh yeah, it's big brain time.

--- renderer/model_ll.c
+++ renderer/model_ll.c
@@ -35,7 +35,7 @@ t_global_vert	*list2vert(t_global_vert *list, int index)
 	return (list);
 }
 
-t_face_vert		*list2face(t_face_vert *list, int index)
+t_face_vert		*list2fvert(t_face_vert *list, int index)
 {
 	if (index > 0)
 	{
@@ -60,83 +60,59 @@ t_face_vert		*list2face(t_face_vert *list, int index)
 	return (list);
 }
 
-void			global_vert_add(t_global_vert *dest, t_global_vert *node)
+t_face_vert		*list2face(t_actual_face *list, int index)
 {
-	t_global_vert *last;
-
-	last = list2vert(dest, -1);
-	last->next = node;
-	node->prev = last;
+	if (index > 0)
+	{
+		while (index-- > 0)
+		{
+			if (list->next != NULL)
+				list = list->next;
+			else return (list);
+		}
+	}
+	else if (index < 0)
+	{
+		while (list->next != NULL)
+			list = list->next;
+		while (++index < 0)
+		{
+			if (list->prev != NULL)
+				list = list->prev;
+			else return (list);
+		}
+	}
+	return (list);
 }
 
-void			face_vert_add(t_face_vert *dest, t_global_vert *node)
+t_global_vert	*global_vert_add(t_global_vert *head, t_global_vert *node)
 {
-	t_face_vert *last;
+	t_global_vert	*last;
 
-	last = list2face(dest, -1);
+	last = list2vert(head, LAST);
 	last->next = node;
 	node->prev = last;
+	return (head);
 }
 
-void			join_face_list(t_face_vert *a, t_face_vert *b)
+t_face_vert		*face_vert_add(t_face_vert *head, t_face_vert *tail)
 {
 	t_face_vert	*last;
 
-	last = list2face(a, -1);
-	last->next = b;
-	b->prev = last;
+	last = list2fvert(head, LAST);
+	last->next = tail;
+	tail->prev = last;
+	return (head);
 }
 
-// adds given verts to the global list
-// creates 3 face nodes which contain pointers to verts in global list
-t_face_vert	*new_face(t_vert a, t_vert b, t_vert c)
+t_actual_face	*face_list_add(t_actual_face *head, t_actual_face *tail)
 {
-	t_global_vert	*current;
-	t_global_vert	*last_vert;
-	t_face_vert		*list_out;
-	t_face_vert		*node;
+	t_actual_face	*last;
 
-	last_vert = list2vert(g_verts, -1);
-	last_vert->next = new_vert(last_vert, a, NULL);
-	assert(node = malloc(sizeof(*node)));
-	node->prev = NULL;
-	node->p = last_vert->next;
-
-	// Ignore this mess:
-
-	// assert(current = malloc(sizeof(*current)));
-	// last->next = current;
-	// 	current->v = a;
-	// 	current->prev = last;
-	// 	current->next = NULL;
-	// assert(face = malloc(sizeof(*face)));
-	// face->prev = NULL;
-	// face->p = current;
-	// last = current;
-
-	// assert(current = malloc(sizeof(*current)));
-	// face->next = current;
-	// last->next = current;
-	// 	current->v = b;
-	// 	current->prev = last;
-	// 	current->next = NULL;
-	// assert(face = malloc(sizeof(*face)));
-	// face->p = current;
-	// face->prev = last;
-	// last = current;
-
-	// assert(current = malloc(sizeof(*current)));
-	// face->next = current;
-	// last->next = current;
-	// 	current->v = c;
-	// 	current->prev = last;
-	// 	current->next = NULL;
-	// assert(face = malloc(sizeof(*face)));
-	// face->p = current;
-	// face->prev = last;
-	// last = current;
-
-	return (list_out);
+	last = list2face(head, LAST);
+	last->next = tail;
+	tail->prev = last;
+	return (head);
 }
 
 t_global_vert	*new_vert(t_global_vert *prev, t_vert v, t_global_vert *next)
@@ -145,20 +121,67 @@ t_global_vert	*new_vert(t_global_vert *prev, t_vert v, t_global_vert *next)
 
 	assert(current = malloc(sizeof(*current)));
 
-	current->v = v;
+	current->pos = v;
 	current->prev = prev;
 	current->next = next;
-	// face->p = current;
-	// face->prev = (*last);
-	// (*last)->next = current;
-	// (*last) = current;
-	// face->next = current;
+	global_vert_add(g_verts, current);
 	return (current);
 }
 
-t_obj	mdl_to_usable_data()
+t_face_vert		*new_fvert(t_face_vert *prev, t_global_vert *data, t_face_vert *next)
 {
-	t_obj			obj;
+	t_face_vert	*current;
+
+	assert(current = malloc(sizeof(*current)));
+
+	current->data = data;
+	current->prev = prev;
+	current->next = next;
+	return (current);
+}
+
+// adds given verts to the global list
+// creates 3 face nodes which contain pointers to verts in global list
+t_actual_face	*new_face(t_vert a, t_vert b, t_vert c)
+{
+	t_actual_face	*out;
+
+	assert(out = malloc(sizeof(*out)));
+
+	out->prev = NULL;
+	out->vert             = new_fvert(NULL,            new_vert(NULL, a, NULL), NULL);
+	out->vert->next       = new_fvert(out->vert,       new_vert(NULL, b, NULL), NULL);
+	out->vert->next->next = new_fvert(out->vert->next, new_vert(NULL, c, NULL), NULL);
+	out->next = NULL;
+	return (out);
+}
+
+// Builds a vertical wall from a 2D line as if viewed top-down.
+// Two verts will be at the same XY coordinate but different Z (height)
+// The third vert will be at the opposite end of the line.
+// A second triangle is made with the same method,
+// only with two points at the opposite end instead.
+t_actual_face	*make_wall(t_wall *a, t_wall *b, int floor, int roof)
+{
+	t_vert			v[3];
+	t_actual_face	*f[2];
+
+	v[0] = vec4(a->start.x, a->start.y, roof, T_POS);
+	v[1] = vec4(a->start.x, a->start.y, floor, T_POS);
+	v[2] = vec4(b->start.x, b->start.y, roof, T_POS);
+	f[0] = new_face(v[0], v[1], v[2]);
+
+	v[0] = vec4(a->start.x, a->start.y, floor, T_POS);
+	v[1] = vec4(b->start.x, b->start.y, floor, T_POS);
+	v[2] = vec4(b->start.x, b->start.y, roof, T_POS);
+	f[1] = new_face(v[0], v[1], v[2]);
+
+	return (face_list_add(f[0], f[1]));
+}
+
+t_obj			mdl_to_usable_data()
+{
+	t_obj			room_object;
 	t_doom			*doom;
 
 	int				wall_count;
@@ -168,17 +191,17 @@ t_obj	mdl_to_usable_data()
 	t_wall			*wall;
 	t_room			*room;
 
-	t_face_vert	*wall_tris;
-	t_face_vert	*single_wall;
+	t_actual_face	*wall_tris;
+	t_actual_face	*single_wall;
 
 	room_count = doom->mdl->room_count;
-	room = doom->mdl->room_first;
+	room       = doom->mdl->room_first;
 	while (room_count--)
 	{
 		floor_height = room->floor_height;
-		roof_height = room->roof_height;
-		wall_count = room->wall_count;
-		wall = room->first_wall;
+		roof_height  = room->roof_height;
+		wall_count   = room->wall_count;
+		wall         = room->first_wall;
 		while (wall_count--)
 		{
 			// line from A -----> B
@@ -194,30 +217,15 @@ t_obj	mdl_to_usable_data()
 				single_wall = make_wall(wall, wall->next, floor_height, roof_height);
 			else
 				single_wall = make_wall(wall, room->first_wall, floor_height, roof_height);
+
 			// join those triangles to some bigger list
-			join_face_list(wall_tris, single_wall);
+			face_list_add(wall_tris, single_wall);
+			room_object.v_count += 6;
+			room_object.f_count += 2;
 			wall = wall->next;
 		}
 		room = room->next;
 	}
-	obj.v_count
+
+	printf("FINAL ROOM STATS: %d verts, %d faces, v:%p, f:%p\n", room_object.v_count, room_object.f_count, room_object.vert, room_object.face);
 }
-
-t_face_vert	*make_wall(t_wall *a, t_wall *b, int floor, int roof)
-{
-	t_vert		v[3];
-	t_face_vert	*f[2];
-
-	v[0] = vec4(a->start.x, a->start.y, roof, T_POS);
-	v[1] = vec4(a->start.x, a->start.y, floor, T_POS);
-	v[2] = vec4(b->start.x, b->start.y, roof, T_POS);
-	f[0] = new_face(v[0], v[1], v[2]);
-	v[0] = vec4(a->start.x, a->start.y, floor, T_POS);
-	v[1] = vec4(b->start.x, b->start.y, floor, T_POS);
-	v[2] = vec4(b->start.x, b->start.y, roof, T_POS);
-	f[0] = new_face(v[0], v[1], v[2]);
-	join_face_list(f[0], f[1]);
-	return (f[0]);
-}
-
-
--- renderer/model_ll.h
+++ renderer/model_ll.h
@@ -1,6 +1,8 @@
 #ifndef MODEL_LL_H
 # define MODEL_LL_H
 
+# define LAST -1
+
 /*
 ** Object
 **	keeps count of its Vert and Face list
@@ -14,6 +16,9 @@
 */
 
 // global list of vertices for the current object
+// s_global_vert->next is the next VERTICE.
+// note: next/prev are only used for ADDING/REMOVING vertices.
+// note: When ACCESSING t_vert.pos, next/prev are meaningless.
 typedef struct	s_global_vert
 {
 	t_vert					pos;
@@ -22,33 +27,67 @@ typedef struct	s_global_vert
 }				t_global_vert;
 
 // list of vertices for the current face
+// s_face_vert->next is the next VERTICE OF THIS FACE.
+// s_face_vert->data is a reference to the global object vert list.
 typedef struct	s_face_vert
 {
-	t_global_vert		*vert;
+	t_global_vert		*data;
 	struct s_face_vert	*prev;
 	struct s_face_vert	*next;
 }				t_face_vert;
 
 // list of faces for the current object
-typedef struct	s_face_node
+// s_actual_face->next is the next FACE
+typedef struct	s_actual_face
 {
-	t_face_vert			*face;
-	struct s_face_node	*prev;
-	struct s_face_node	*next;
-}				t_face_node;
+	t_face_vert				*vert;
+	struct s_actual_face	*prev;
+	struct s_actual_face	*next;
+}				t_actual_face;
 
 typedef struct	s_obj
 {
 	int				v_count;
 	int				f_count;
-	t_global_vert	*v_list;
-	t_face_node		*f_list;
+	t_global_vert	*vert;
+	t_actual_face	*face;
 }				t_obj;
 
-// obj object;
-// t_vert f1v1 = object->f_list-> face-> vert.pos;
-// t_vert f2v2 = object->f_list-> face->next-> vert->next.pos;
+//       Todo: Is this really needed? | Memory efficiency?
+//     What do we lose if we drop it? | Duplicate/redundant data?
+//            Why can't it be t_vert? | Freeing the pointer might segfault...
+//                                    |   * Correction: global verts (aka the memory being referred to) should not be deleted during the frame.
+//                                    |   * Extra verts should be discarded at the end of the frame when nothing is needed anymore.
+//                                    | Isn't this already redundant?
+//                                    | Removing this would free at least 24 (50%) bytes.
+//                                    | Would the global list be redundant too then?
+//                                    |   * It can't be just t_vert because then t_actual_face could only have one t_vert.
+//                                    |   * It can't be a fixed size array because we can't determine how many new verts we'll need.
+//                                    |     * Worst case can be over 10 total verts. Too much memory to reserve for all faces.
+//                                    |   * Therefore it must be a linked list or realloced with a count?
+//                                    |   * Reallocing might be complicated if verts get removed from the middle of the block.
+//                                    |   * A linked list with count? Should've done that in the first place, but it's not a definite improvement.
+//                                    |     * Linked list with count requires every link to have their counts updated...
+//                                    |     * Unless a special "head" struct is added to hold some metadata... More stuff to manage...
+//                                    |
+// t_obj object;         ↓ face       ↓ vert-ref   ↓ vert-data
+// t_vert f1v1 = object->face->       vert->       data.pos;
+// t_vert f2v2 = object->face->next-> vert->next-> data.pos;
 
 # include "renderer.h"
+
+t_global_vert	*new_vert(t_global_vert *prev, t_vert v, t_global_vert *next);
+t_face_vert		*new_fvert(t_face_vert *prev, t_global_vert *data, t_face_vert *next);
+t_actual_face	*new_face(t_vert a, t_vert b, t_vert c);
+
+t_global_vert	*list2vert(t_global_vert *list, int index);
+t_face_vert		*list2fvert(t_face_vert *list, int index);
+t_face_vert		*list2face(t_actual_face *list, int index);
+
+t_global_vert	*global_vert_add(t_global_vert *head, t_global_vert *node);
+t_face_vert		*face_vert_add(t_face_vert *head, t_face_vert *tail);
+t_actual_face	*face_list_add(t_actual_face *head, t_actual_face *tail);
+
+t_actual_face	*make_wall(t_wall *a, t_wall *b, int floor, int roof);
 
 #endif
