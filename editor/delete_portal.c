892139a5c25140a19e521e2588182f006be0bcca
Kalle Rustholkarhu <krusthol@student.hive.fi>
2021-01-29 19:25:56 +0200

    Deleting rooms portals now when deleting a room. Inverted mouse look Y axis.

--- editor/delete_portal.c
+++ editor/delete_portal.c
@@ -1,12 +1,62 @@
 #include "doom-nukem.h"
 
-void			delete_portals_by_room(t_room *room, t_model *mdl)
+static int		equal_points(t_point a, t_point b)
 {
-	// Add deletion of portals in a room;
-	return ;
+	if (a.x == b.x && a.y == b.y)
+		return (1);
+	return (0);
 }
 
-static void		update_remaining_portals(int id, t_model *mdl)
+static int		matching_walls(t_wall *a, t_wall *b)
+{
+	if (equal_points(a->start, b->start) && equal_points(a->end, b->end))
+		return (1);
+	if (equal_points(a->start, b->end) && equal_points(a->end, b->start))
+		return (1);
+	return (0);
+}
+
+static int		portal_belongs_to_room(t_wall *portal, t_room *room)
+{
+	t_wall	*wall;
+	int 	wc;
+
+	wall = room->first_wall;
+	wc = room->wall_count;
+	while (wc--)
+	{
+		if (matching_walls(portal, wall))
+			return (1);
+		wall = wall->next;
+	}
+	return (0);
+}
+
+void			delete_portals_by_room(t_room *room, t_model *mdl)
+{
+	t_wall	*portal;
+	int		pc;
+	int			debug_id;
+
+	debug_model_portals();
+	ft_putendl("\nDeleting portals by room\n");
+	pc = mdl->portal_count;
+	portal = mdl->portal_first;
+		debug_id = 0;
+	while (pc--)
+	{
+		if (portal_belongs_to_room(portal, room))
+		{
+			delete_portal(portal, mdl);
+				printf("deleted portal id %d\n", debug_id);
+		}
+		portal = portal->next;
+			debug_id++;
+	}
+	debug_model_portals();
+}
+
+static void		update_remaining_portals(t_wall *delete, t_model *mdl)
 {
 	t_wall	*portal;
 	t_wall	*relink;
@@ -14,21 +64,27 @@ static void		update_remaining_portals(int id, t_model *mdl)
 
 	pc = mdl->portal_count;
 	portal = mdl->portal_first;
-	if (id == 0 && mdl->portal_count > 1)
-		mdl->portal_first = mdl->portal_first->next;
+	if (mdl->portal_first == delete && mdl->portal_count > 1)
+		mdl->portal_first = delete->next;
 	while (pc--)
 	{
-		if (portal->id == id - 1)
+		if (portal->id == delete->id - 1)
+		{
 			relink = portal;
-		if (portal->id == id + 1)
+			relink->next = NULL;
+		}
+		if (portal->id == delete->id + 1)
 			relink->next = portal;
+		portal->id--;
 		portal = portal->next;
 	}
+	if (relink->next == NULL)
+		relink->next = mdl->portals;
 }
 
 void			delete_portal(t_wall *portal, t_model *mdl)
 {
-	update_remaining_portals(portal->id, mdl);
+	update_remaining_portals(portal, mdl);
 	mdl->portal_count--;
 	free(portal);
 }
