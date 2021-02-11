7e9632a802aafef8b17402306799205ef9110614
MuSuareZ <murillo.finlandia@gmail.com>
2021-02-11 18:19:48 +0200

    Added some test textures for walls and floor. Improved player pos validation prototype, still WIP

--- game/player_movement2.c
+++ game/player_movement2.c
@@ -6,7 +6,7 @@
 /*   By: msuarez- <msuarez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 19:07:12 by msuarez-          #+#    #+#             */
-/*   Updated: 2021/02/10 17:07:16 by msuarez-         ###   ########.fr       */
+/*   Updated: 2021/02/11 18:18:14 by msuarez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,38 @@ void			init_player_z(t_doom *doom)
 	doom->mdl->player.z = room_by_id(room_id)->floor_height + doom->mdl->player.height;
 }
 
+static int		check_eight_pos_points(t_doom *doom)
+{
+	t_coord		loc_id[8];
+	int			i;
+
+	loc_id[0].x = doom->mdl->player.x + 10;
+	loc_id[0].y = doom->mdl->player.y;
+	loc_id[1].x = doom->mdl->player.x - 10;
+	loc_id[1].y = doom->mdl->player.y;
+	loc_id[2].x = doom->mdl->player.x;
+	loc_id[2].y = doom->mdl->player.y - 10;
+	loc_id[3].x = doom->mdl->player.x;
+	loc_id[3].y = doom->mdl->player.y + 10;
+
+	loc_id[4].x = doom->mdl->player.x + 7;
+	loc_id[4].y = doom->mdl->player.y - 7;
+	loc_id[5].x = doom->mdl->player.x - 7;
+	loc_id[5].y = doom->mdl->player.y - 7;
+	loc_id[6].x = doom->mdl->player.x + 7;
+	loc_id[6].y = doom->mdl->player.y + 7;
+	loc_id[7].x = doom->mdl->player.x - 7;
+	loc_id[7].y = doom->mdl->player.y + 7;
+	i = 0;
+	while (i < 8)
+	{
+		if (check_location(doom, loc_id[i].x, loc_id[i].y) == -1)
+			return (-1);
+		i++;
+	}
+	return (0);
+}	
+
 void			validate_player_position(t_doom *doom, t_coord old)
 {
 	static	int	last_room_id = -1;
@@ -29,8 +61,7 @@ void			validate_player_position(t_doom *doom, t_coord old)
 	location_id = check_location(doom, doom->mdl->player.x,
 								 doom->mdl->player.y);
 	if (location_id == -1 || location_id == UINT_ERROR_CONSTANT ||
-		player_collision_with_enemies(doom) == -1 ||
-		check_location(doom, doom->mdl->player.safety_spot.x, doom->mdl->player.safety_spot.y) == -1)
+		player_collision_with_enemies(doom) == -1 || check_eight_pos_points(doom) == -1)
 	{
 		doom->mdl->player.x = old.x;
 		doom->mdl->player.y = old.y;
