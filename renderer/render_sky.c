f6ff7a9ffd8e42f20dbb019e783c4abe1f77cec0
Niko Gontjarow <niko.gontjarow@gmail.com>
2021-04-24 19:00:02 +0300

    Fixed skybox conditions, renamed variables.

--- renderer/render_sky.c
+++ renderer/render_sky.c
@@ -11,55 +11,55 @@ void	render_sky(t_doom *doom)
 	int	angle	= get_model()->player.rot - 180;
 	int	start	= (TEX_CENTER_ANGLE + (angle - FOV_BY_TWO));
 	int stop	= (TEX_CENTER_ANGLE + (angle + FOV_BY_TWO));
+	printf("\n\n");
 
 	{
 		// printf("normal blit      (angle %4i start %4i stop %4i)\n", angle, start, stop);
 
-		SDL_Rect	dstrect;
-		dstrect.x = 0;
-		dstrect.y = 0;
-		dstrect.w = GAME_WIN_WIDTH;
-		dstrect.h = GAME_WIN_HEIGHT;
+		SDL_Rect	screen;
+		screen.x = 0;
+		screen.y = 0;
+		screen.w = GAME_WIN_WIDTH;
+		screen.h = GAME_WIN_HEIGHT;
 
-		SDL_Rect	srcrect = dstrect;
-		srcrect.x = start * pixels_per_angle;
-		SDL_BlitSurface(bg, &srcrect, doom->game->buff, &dstrect);
+		SDL_Rect	image = screen;
+		image.x = start * pixels_per_angle;
+		SDL_BlitSurface(bg, &image, doom->game->buff, &screen);
 	}
 
 	if (start < 0)
 	{
+		// printf("start < 0        (angle %4i start %4i stop %4i)\n", angle, start, stop);
 		start += 360;
 		stop = 360;
-		// printf("start < 0        (angle %4i start %4i stop %4i)\n", angle, start, stop);
 
-		SDL_Rect	dstrect;
-		dstrect.x = 0;
-		dstrect.y = 0;
-		dstrect.w = GAME_WIN_WIDTH;
-		dstrect.h = GAME_WIN_HEIGHT;
+		SDL_Rect	screen;
+		screen.x = 0;
+		screen.y = 0;
+		screen.w = GAME_WIN_WIDTH;
+		screen.h = GAME_WIN_HEIGHT;
 
-		SDL_Rect	srcrect = dstrect;
-		srcrect.x = start * pixels_per_angle;
-		srcrect.w = (stop - start) * pixels_per_angle;
-		// printf("                 (x %4i, w %4i) (x %4i)\n", srcrect.x, srcrect.w, dstrect.x);
-		SDL_BlitSurface(bg, &srcrect, doom->game->buff, &dstrect);
+		SDL_Rect	image = screen;
+		image.x = start * pixels_per_angle;
+		image.w = (stop - start) * pixels_per_angle;
+		// printf("                 (x %4i, w %4i) (x %4i)\n", image.x, image.w, screen.x);
+		SDL_BlitSurface(bg, &image, doom->game->buff, &screen);
 	}
-
-	if (stop > 360 - FOV_BY_TWO)
+	else if (stop > 360 - FOV_BY_TWO)
 	{
-		start -= 360;
 		// printf("stop > 315       (angle %4i start %4i stop %4i)\n", angle, start, stop);
+		start -= 360;
 
-		SDL_Rect	dstrect;
-		dstrect.x = 0;
-		dstrect.y = 0;
-		dstrect.w = GAME_WIN_WIDTH;
-		dstrect.h = GAME_WIN_HEIGHT;
+		SDL_Rect	screen;
+		screen.x = 0;
+		screen.y = 0;
+		screen.w = GAME_WIN_WIDTH;
+		screen.h = GAME_WIN_HEIGHT;
 
-		SDL_Rect	srcrect = dstrect;
-		srcrect.x = start * pixels_per_angle;
-		srcrect.w = (stop - start) * pixels_per_angle;
-		// printf("                 (x %4i, w %4i) (x %4i)\n", srcrect.x, srcrect.w, dstrect.x);
-		SDL_BlitSurface(bg, &srcrect, doom->game->buff, &dstrect);
+		SDL_Rect	image = screen;
+		image.x = start * pixels_per_angle;
+		image.w = (stop - start) * pixels_per_angle;
+		// printf("                 (x %4i, w %4i) (x %4i)\n", image.x, image.w, screen.x);
+		SDL_BlitSurface(bg, &image, doom->game->buff, &screen);
 	}
 }
